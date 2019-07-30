fdf2f285e09958f13ae59f7a4fa9c41188e216f1
Steven Galvin <steven.john.galvin@gmail.com>
2019-07-30 11:51:35 -0700

    add function to hide playlist on click

--- l/assets/scripts/amplitude/functions.js
+++ l/assets/scripts/amplitude/functions.js
@@ -1,13 +1,25 @@
 window.onload = function() {
 
+  // Show list screen on click
+
   document.getElementsByClassName('down-header')[0].addEventListener('click', function() {
     var list = document.getElementById('list');
 
-    // list.style.height = ( parseInt( document.getElementById('audio-player').offsetHeight ) - 135 ) + 'px';
+    list.style.height = ( parseInt( document.getElementsByClassName('audio-container')[0].offsetHeight ) - 135 ) + 'px';
 
     document.getElementById('list-screen').classList.remove('slide-out-top');
     document.getElementById('list-screen').classList.add('slide-in-top');
     document.getElementById('list-screen').style.display = "block";
+  });
+
+  // -- Show list screen on click --
+
+  // Hide list screen on click
+
+  document.getElementsByClassName('hide-playlist')[0].addEventListener('click', function() {
+    document.getElementById('list-screen').classList.remove("slide-in-top");
+    document.getElementById('list-screen').classList.add("slide-out-top");
+    document.getElementById('list-screen').style.display = "none";
   });
 
 }
