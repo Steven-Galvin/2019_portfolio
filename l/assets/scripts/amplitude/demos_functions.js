5f22df377fc1044615b85316701056d893d83391
Steven Galvin <steven.john.galvin@gmail.com>
2019-07-30 12:33:40 -0700

    relabel code in progress as old. download code from Amplitude examples.

---
+++ l/assets/scripts/amplitude/demos_functions.js
@@ -0,0 +1,42 @@
+// Project: Personal Music
+// Programmer: Steven Galvin
+// Filename: demos_init.html
+// Date Modified: 07/29/19
+
+window.onkeydown = function(e) {
+    return !(e.keyCode == 32);
+};
+
+/*
+  Handles a click on the down button to slide down the playlist.
+*/
+document.getElementsByClassName('down-header')[0].addEventListener('click', function(){
+  var list = document.getElementById('list');
+
+  list.style.height = ( parseInt( document.getElementById('flat-black-player-container').offsetHeight ) - 135 ) + 'px';
+
+  document.getElementById('list-screen').classList.remove('slide-out-top');
+  document.getElementById('list-screen').classList.add('slide-in-top');
+  document.getElementById('list-screen').style.display = "block";
+});
+
+/*
+  Handles a click on the up arrow to hide the list screen.
+*/
+document.getElementsByClassName('hide-playlist')[0].addEventListener('click', function(){
+  document.getElementById('list-screen').classList.remove('slide-in-top');
+  document.getElementById('list-screen').classList.add('slide-out-top');
+  document.getElementById('list-screen').style.display = "none";
+});
+
+/*
+  Handles a click on the song played progress bar.
+*/
+document.getElementById('song-played-progress').addEventListener('click', function( e ){
+  var offset = this.getBoundingClientRect();
+  var x = e.pageX - offset.left;
+
+  Amplitude.setSongPlayedPercentage( ( parseFloat( x ) / parseFloat( this.offsetWidth) ) * 100 );
+});
+
+document.querySelector('img[data-amplitude-song-info="cover_art_url"]').style.height = document.querySelector('img[data-amplitude-song-info="cover_art_url"]').offsetWidth + 'px';
--- l/assets/scripts/amplitude/demos_init.js
+++ l/assets/scripts/amplitude/demos_init.js
@@ -1,3 +1,8 @@
+// Project: Personal Music
+// Programmer: Steven Galvin
+// Filename: demos_init.html
+// Date Modified: 07/29/19
+
 Amplitude.init({
   "bindings": {
       39: "next",
@@ -12,56 +17,56 @@ Amplitude.init({
         "name": "NF Type Beat",
         "artist": "Jay Kalsifer",
         "album": "Beats: Mood Swings",
-        "url": "../../assets/audio/nf_type_beat.mp3",
-        "cover": "../../assets/images/perspective.jpg",
+        "url": "../assets/audio/nf_type_beat.mp3",
+        "cover_art_url": "../assets/images/perspective.jpg",
         "type": "Beat"
     },
     {
         "name": "Intro Beat",
         "artist": "Jay Kalsifer",
         "album": "Beats: Mood Swings",
-        "url": "../../assets/audio/intro_beat.mp3",
-        "cover": "../../assets/images/perspective.jpg",
+        "url": "../assets/audio/intro_beat.mp3",
+        "cover_art_url": "../assets/images/perspective.jpg",
         "type": "Beat"
     },
     {
         "name": "Dark Strings Beat",
         "artist": "Jay Kalsifer",
         "album": "Beats: Mood Swings",
-        "url": "../../assets/audio/dark_strings_beat.mp3",
-        "cover": "../../assets/images/perspective.jpg",
+        "url": "../assets/audio/dark_strings_beat.mp3",
+        "cover_art_url": "../assets/images/perspective.jpg",
         "type": "Beat"
     },
     {
         "name": "Lo-Fi Beat",
         "artist": "Jay Kalsifer",
         "album": "Beats: Mood Swings",
-        "url": "../../assets/audio/lofi_beat.mp3",
-        "cover": "../../assets/images/perspective.jpg",
+        "url": "../assets/audio/lofi_beat.mp3",
+        "cover_art_url": "../assets/images/perspective.jpg",
         "type": "Beat"
     },
     {
         "name": "Basement Bounce",
         "artist": "Jay Kalsifer",
         "album": "Beats: Mood Swings",
-        "url": "../../assets/audio/basement_bounce.mp3",
-        "cover": "../../assets/images/perspective.jpg",
+        "url": "../assets/audio/basement_bounce.mp3",
+        "cover_art_url": "../assets/images/perspective.jpg",
         "type": "Beat"
     },
     {
         "name": "Errybody Verse",
         "artist": "Big Joe & Jay Kalsifer",
         "album": "Beats: Mood Swings",
-        "url": "../../assets/audio/errybody_verse.mp3",
-        "cover": "../../assets/images/trigger_warning.jpg",
+        "url": "../assets/audio/errybody_verse.mp3",
+        "cover_art_url": "../assets/images/trigger_warning.jpg",
         "type": "Demo"
     },
     {
         "name": "Livin' Legend Demo",
         "artist": "Big Joe & Jay Kalsifer",
         "album": "Beats: Mood Swings",
-        "url": "../../assets/audio/livin_legend_demo.mp3",
-        "cover": "../../assets/images/trigger_warning.jpg",
+        "url": "../assets/audio/livin_legend_demo.mp3",
+        "cover_art_url": "../assets/images/trigger_warning.jpg",
         "type": "Demo"
     }
   ],
